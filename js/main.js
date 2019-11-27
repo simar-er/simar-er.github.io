a624a31b9fde38a772dc660cceea00d743c9633b
erwinyani <vendettaajah@gmail.com>
2019-11-27 14:31:43 +0700

    updates, rev3

--- js/main.js
+++ js/main.js
@@ -1,119 +1,481 @@
-/*
-	Prologue by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+/*-----------------------------------------------------------------------------------
+
+  Template Name: Uniqlo-Minimalist eCommerce HTML5 Template.
+  Template URI: #
+  Description: Uniqlo is a unique website template designed in HTML with a simple & beautiful look. There is an excellent solution for creating clean, wonderful and trending material design corporate, corporate any other purposes websites.
+  Author: HasTech
+  Author URI: https://themeforest.net/user/hastech/portfolio
+  Version: 1.1
+
+-----------------------------------------------------------------------------------*/
+
+/*-------------------------------
+[  Table of contents  ]
+---------------------------------
+  01. jQuery MeanMenu
+  02. wow js active
+  03. Portfolio  Masonry (width)
+  04. Sticky Header
+  05. ScrollUp
+  06. Tooltip
+  07. ScrollReveal Js Init
+  08. Fixed Footer bottom script ( Newsletter )
+  09. Search Bar
+  10. Toogle Menu
+  11. Shopping Cart Area
+  12. Filter Area
+  13. User Menu
+  14. Overlay Close
+  15. Home Slider 
+  16. Popular Product Wrap
+  17. Testimonial Wrap
+  18. Magnific Popup  
+  19. Price Slider Active
+  20. Plus Minus Button
+  21. jQuery scroll Nav
+
+  
+
+/*--------------------------------
+[ End table content ]
+-----------------------------------*/
+
 
 (function($) {
+    'use strict';
 
-	skel.breakpoints({
-		wide: '(min-width: 961px) and (max-width: 1880px)',
-		normal: '(min-width: 961px) and (max-width: 1620px)',
-		narrow: '(min-width: 961px) and (max-width: 1320px)',
-		narrower: '(max-width: 960px)',
-		mobile: '(max-width: 736px)'
-	});
 
-	$(function() {
+/*-------------------------------------------
+  01. jQuery MeanMenu
+--------------------------------------------- */
+    
+$('.mobile-menu nav').meanmenu({
+    meanMenuContainer: '.mobile-menu-area',
+    meanScreenWidth: "991",
+    meanRevealPosition: "right",
+});
 
-		var	$window = $(window),
-			$body = $('body');
+/*-------------------------------------------
+  02. wow js active
+--------------------------------------------- */
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+  new WOW().init();
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
 
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+/*-------------------------------------------
+  03. Product  Masonry (width)
+--------------------------------------------- */ 
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
+$('.htc__product__container').imagesLoaded( function() {
+  
+    // filter items on button click
+    $('.product__menu').on( 'click', 'button', function() {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
+    }); 
+    // init Isotope
+    var $grid = $('.product__list').isotope({
+      itemSelector: '.single__pro',
+      percentPosition: true,
+      transitionDuration: '0.7s',
+      layoutMode: 'fitRows',
+      masonry: {
+        // use outer width of grid-sizer for columnWidth
+        columnWidth: 1,
+      }
+    });
 
-		// Scrolly links.
-			$('.scrolly').scrolly();
+});
 
-		// Nav.
-			var $nav_a = $('#nav a');
+$('.product__menu button').on('click', function(event) {
+    $(this).siblings('.is-checked').removeClass('is-checked');
+    $(this).addClass('is-checked');
+    event.preventDefault();
+});
 
-			// Scrolly-fy links.
-				$nav_a
-					.scrolly()
-					.on('click', function(e) {
 
-						var t = $(this),
-							href = t.attr('href');
 
-						if (href[0] != '#')
-							return;
+/*-------------------------------------------
+  04. Sticky Header
+--------------------------------------------- */ 
 
-						e.preventDefault();
 
-						// Clear active and lock scrollzer until scrolling has stopped
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
+  var win = $(window);
+  var sticky_id = $("#sticky-header-with-topbar");
+  win.on('scroll',function() {    
+    var scroll = win.scrollTop();
+    if (scroll < 245) {
+      sticky_id.removeClass("scroll-header");
+    }else{
+      sticky_id.addClass("scroll-header");
+    }
+  });
 
-						// Set this link to active
-							t.addClass('active');
 
-					});
 
-			// Initialize scrollzer.
-				var ids = [];
 
-				$nav_a.each(function() {
 
-					var href = $(this).attr('href');
+/*--------------------------
+  05. ScrollUp
+---------------------------- */
+$.scrollUp({
+    scrollText: '<i class="zmdi zmdi-chevron-up"></i>',
+    easingType: 'linear',
+    scrollSpeed: 900,
+    animation: 'fade'
+});
 
-					if (href[0] != '#')
-						return;
 
-					ids.push(href.substring(1));
 
-				});
+/*---------------------------
+  06. Tooltip
+------------------------------*/    
+$('[data-toggle="tooltip"]').tooltip({
+    animated: 'fade',
+    placement: 'top',
+    container: 'body'
+});
 
-				$.scrollzer(ids, { pad: 200, lastHack: true });
 
-		// Header (narrower + mobile).
 
-			// Toggle.
-				$(
-					'<div id="headerToggle">' +
-						'<a href="#header" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+/*-----------------------------------
+  07. ScrollReveal Js Init
+-------------------------------------- */
 
-			// Header.
-				$('#header')
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'header-visible'
-					});
+  window.sr = ScrollReveal({ duration: 800 , reset: false });
+    sr.reveal('.foo');
+    sr.reveal('.bar');
 
-			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#headerToggle, #header, #main')
-						.css('transition', 'none');
 
-	});
+
+/*-------------------------------------------------------
+  08. Fixed Footer bottom script ( Newsletter )
+--------------------------------------------------------*/
+
+var $newsletter_height = $(".htc__foooter__area");
+$('.fixed__footer').css({'margin-bottom': $newsletter_height.height() + 'px'});
+
+
+
+
+/*------------------------------------    
+  09. Search Bar
+--------------------------------------*/ 
+    
+  $( '.search__open' ).on( 'click', function () {
+    $( 'body' ).toggleClass( 'search__box__show__hide' );
+    return false;
+  });
+
+  $( '.search__close__btn .search__close__btn_icon' ).on( 'click', function () {
+    $( 'body' ).toggleClass( 'search__box__show__hide' );
+    return false;
+  });
+
+
+
+
+/*------------------------------------    
+  10. Toogle Menu
+--------------------------------------*/
+
+  $('.toggle__menu').on('click', function() {
+    $('.offsetmenu').addClass('offsetmenu__on');
+    $('.body__overlay').addClass('is-visible');
+
+  });
+
+  $('.offsetmenu__close__btn').on('click', function() {
+      $('.offsetmenu').removeClass('offsetmenu__on');
+      $('.body__overlay').removeClass('is-visible');
+  });
+
+
+
+/*------------------------------------    
+  11. Shopping Cart Area
+--------------------------------------*/
+
+  $('.cart__menu').on('click', function() {
+    $('.shopping__cart').addClass('shopping__cart__on');
+    $('.body__overlay').addClass('is-visible');
+
+  });
+
+  $('.offsetmenu__close__btn').on('click', function() {
+      $('.shopping__cart').removeClass('shopping__cart__on');
+      $('.body__overlay').removeClass('is-visible');
+  });
+
+
+/*------------------------------------    
+  12. Filter Area
+--------------------------------------*/
+
+  $('.filter__menu').on('click', function() {
+    $('.filter__wrap').addClass('filter__menu__on');
+    $('.body__overlay').addClass('is-visible');
+
+  });
+
+  $('.filter__menu__close__btn').on('click', function() {
+      $('.filter__wrap').removeClass('filter__menu__on');
+      $('.body__overlay').removeClass('is-visible');
+  });
+
+
+
+/*------------------------------------    
+  13. User Menu
+--------------------------------------*/
+
+  $('.user__menu').on('click', function() {
+    $('.user__meta').addClass('user__meta__on');
+    $('.body__overlay').addClass('is-visible');
+
+  });
+
+  $('.offsetmenu__close__btn').on('click', function() {
+      $('.user__meta').removeClass('user__meta__on');
+      $('.body__overlay').removeClass('is-visible');
+  });
+
+
+
+/*------------------------------------    
+  14. Overlay Close
+--------------------------------------*/
+  $('.body__overlay').on('click', function() {
+    $(this).removeClass('is-visible');
+    $('.offsetmenu').removeClass('offsetmenu__on');
+    $('.shopping__cart').removeClass('shopping__cart__on');
+    $('.filter__wrap').removeClass('filter__menu__on');
+    $('.user__meta').removeClass('user__meta__on');
+  });
+
+
+
+/*-----------------------------------------------
+  15. Home Slider
+-------------------------------------------------*/
+
+  if ($('.slider__activation__wrap').length) {
+    $('.slider__activation__wrap').owlCarousel({
+      loop: true,
+      margin:0,
+      nav:true,
+      autoplay: false,
+      navText: [ '<i class="zmdi zmdi-chevron-left"></i>', '<i class="zmdi zmdi-chevron-right"></i>' ],
+      autoplayTimeout: 10000,
+      items:1,
+      dots: false,
+      lazyLoad: true,
+      responsive:{
+        0:{
+          items:1
+        },
+        1920:{
+          items:1
+        }
+      }
+    });
+  }
+
+  if ($('.slider__activation__02').length) {
+    $('.slider__activation__02').owlCarousel({
+      loop: true,
+      margin:0,
+      nav:false,
+      autoplay: false,
+      autoplayTimeout: 10000,
+      items:1,
+      dots: false,
+      lazyLoad: true,
+      responsive:{
+        0:{
+          items:1
+        },
+        1920:{
+          items:1
+        }
+      }
+    });
+  }
+
+
+
+
+/*-----------------------------------------------
+  16. Popular Product Wrap
+-------------------------------------------------*/
+
+
+  $('.popular__product__wrap').owlCarousel({
+      loop: true,
+      margin:0,
+      nav:true,
+      autoplay: false,
+      navText: [ '<i class="zmdi zmdi-chevron-left"></i>', '<i class="zmdi zmdi-chevron-right"></i>' ],
+      autoplayTimeout: 10000,
+      items:3,
+      dots: false,
+      lazyLoad: true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:2
+        },
+        800:{
+          items:2
+        },
+        1024:{
+          items:3
+        },
+        1200:{
+          items:3
+        },
+        1400:{
+          items:3
+        },
+        1920:{
+          items:3
+        }
+      }
+    });
+
+
+
+/*-----------------------------------------------
+  17. Testimonial Wrap
+-------------------------------------------------*/
+
+
+  $('.testimonial__wrap').owlCarousel({
+      loop: true,
+      margin:0,
+      nav:false,
+      autoplay: false,
+      navText: false,
+      autoplayTimeout: 10000,
+      items:1,
+      dots: false,
+      lazyLoad: true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:1
+        },
+        800:{
+          items:1
+        },
+        1024:{
+          items:1
+        },
+        1200:{
+          items:1
+        },
+        1400:{
+          items:1
+        },
+        1920:{
+          items:1
+        }
+      }
+    });
+
+
+
+
+/*--------------------------------
+  18. Magnific Popup
+----------------------------------*/
+
+$('.video-popup').magnificPopup({
+  type: 'iframe',
+  mainClass: 'mfp-fade',
+  removalDelay: 160,
+  preloader: false,
+  zoom: {
+      enabled: true,
+  }
+});
+
+$('.image-popup').magnificPopup({
+  type: 'image',
+  mainClass: 'mfp-fade',
+  removalDelay: 100,
+  gallery:{
+      enabled:true, 
+  }
+});
+
+
+
+
+
+/*-------------------------------
+  19. Price Slider Active
+--------------------------------*/
+  $("#slider-range").slider({
+      range: true,
+      min: 10,
+      max: 500,
+      values: [110, 400],
+      slide: function(event, ui) {
+          $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+      }
+  });
+  $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+      " - $" + $("#slider-range").slider("values", 1));
+
+
+
+
+/*-------------------------------
+  20.  Plus Minus Button 
+--------------------------------*/
+
+
+    $(".cart-plus-minus").append('<div class="dec qtybutton">-</i></div><div class="inc qtybutton">+</div>');
+
+    $(".qtybutton").on("click", function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();
+        if ($button.text() == "+") {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 1) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 1;
+            }
+        }
+        $button.parent().find("input").val(newVal);
+    });
+
+
+/*--------------------------
+  21. jQuery scroll Nav
+---------------------------- */
+    $('.onepage--menu').onePageNav({
+        scrollOffset: 0
+    }); 
+
+
+
+
+
+
+
 
 })(jQuery);
+
+
+
+
